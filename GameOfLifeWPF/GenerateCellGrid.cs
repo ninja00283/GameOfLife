448d4717f87529babdb825b92ad849bdeeacea49
jan bos <max.bos217@gmail.com>
2019-05-16 14:47:59 +0200

    Added wpf view(to slow)

---
+++ GameOfLifeWPF/GenerateCellGrid.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+using System.Diagnostics;
+
+namespace GameOfLifeWPF
+{
+    class GenerateCellGrid
+    {
+
+
+
+        public static List<List<Rectangle>> StartGenerating(int width, int height,float size) {
+            List<List<Rectangle>> rectangles = new List<List<Rectangle>>();
+            for (int y = 0; y < height; y++)
+            {
+                rectangles.Add(new List<Rectangle>());
+                ((MainWindow)Application.Current.MainWindow).CellGrid.RowDefinitions.Add(new RowDefinition());
+                ((MainWindow)Application.Current.MainWindow).CellGrid.RowDefinitions[y].Height = new GridLength(1, GridUnitType.Star);
+                for (int x = 0; x < width; x++)
+                {
+                    if (y == 0) {
+                        ((MainWindow)Application.Current.MainWindow).CellGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                        ((MainWindow)Application.Current.MainWindow).CellGrid.ColumnDefinitions[x].Width = new GridLength(1, GridUnitType.Star);
+                    }
+
+                    Rectangle rectangle = GenerateRectangle(x,y, size);
+                    rectangle.MouseDown += new MouseButtonEventHandler(CellClick);
+                    Grid.SetRow(rectangle, y+1);
+                    Grid.SetColumn(rectangle, x);
+                    ((MainWindow)Application.Current.MainWindow).CellGrid.Children.Add(rectangle);
+                    rectangles[y].Add(rectangle);
+                }
+            }
+            return rectangles;
+        }
+
+        public static Rectangle GenerateRectangle(int x, int y, float size)
+        {
+
+            return new Rectangle()
+            {
+                Height = Double.NaN,
+                Width = Double.NaN,
+                Fill = Brushes.LightGray,
+                Stroke = Brushes.LightGray,
+                Name = "x" + x.ToString() + "y" + y.ToString() + "Rectangle",
+                Focusable = false
+
+            };
+        }
+
+
+        public static void CellClick(object sender, EventArgs e)
+        {
+            if ((sender as Rectangle).Fill == Brushes.Orange)
+            {
+                (sender as Rectangle).Fill = Brushes.LightGray;
+            }
+            else
+            {
+                (sender as Rectangle).Fill = Brushes.Orange;
+            }
+        }
+    }
+}
--- GameOfLifeWPF/MainWindow.xaml.cs
+++ GameOfLifeWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,10 +22,78 @@ namespace GameOfLifeWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        int size = 50;
+
+        List<List<Rectangle>> rectangles;
+
+        public  MainWindow()
         {
-            LifeLogic.BeginCheckingCells(new GameBoardClass().GameBoardGrid);
+            Debug.WriteLine("loop");
             InitializeComponent();
+
+            buttonupdate.Click += UpdateGrid;
+
+
+            rectangles = GenerateCellGrid.StartGenerating(size, size, 4.5f);
+        }
+
+
+
+        private void UpdateGrid(object sender, RoutedEventArgs e)
+        {
+
+                Debug.WriteLine("keydown");
+                ReadAndWriteGrid readGrid = new ReadAndWriteGrid();
+                List<List<char>> CellGrid = LifeLogic.BeginCheckingCells(readGrid.ReadWholeGrid(size, rectangles));
+                readGrid.WriteGrid(CellGrid, rectangles);
+        }
+        
+    }
+
+    class ReadAndWriteGrid
+    {
+        public List<List<char>> ReadWholeGrid(int size, List<List<Rectangle>> rectangles) {
+            List<List<char>> CellGrid = new List<List<char>>();
+            for (int y = 0; y < size; y++)
+            {
+                CellGrid.Add(new List<char>());
+                for (int x = 0; x < size; x++)
+                {
+                    Debug.WriteLine("loop");
+                    if (rectangles[y][x].Fill == Brushes.Orange)
+                    {
+                        CellGrid[y].Add('X');
+                    }
+                    else
+                    {
+                        CellGrid[y].Add('0');
+                    }
+
+                }
+            }
+            return CellGrid;
+        }
+
+        public void WriteGrid(List<List<char>>  CellGrid, List<List<Rectangle>> rectangles) {
+            for (int y = 0; y < CellGrid.Count; y++)
+            {
+                List<char> yItem = CellGrid[y];
+                for (int x = 0; x < yItem.Count; x++)
+                {
+                    char xItem = yItem[x];
+                    Rectangle rectangle = rectangles[y][x];
+                    if (xItem == 'X')
+                    {
+                        rectangle.Fill = Brushes.Orange;
+                    }
+                    else
+                    {
+                        rectangle.Fill = Brushes.LightGray;
+                    }
+                }
+            }
         }
     }
 }
+
+
